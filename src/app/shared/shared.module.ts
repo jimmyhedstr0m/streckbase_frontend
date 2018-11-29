d2e2b3aa442d5e61b416214aeb8b7fbf8a68d5a6
Jimmy Hedström <jimmy.hedstrom93@gmail.com>
2018-11-29 22:39:10 +0100

    Added button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { ButtonComponent } from "./button/button.component";
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
   declarations: [
-
+    ButtonComponent
   ],
   exports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ButtonComponent
   ]
 })
 export class SharedModule {
