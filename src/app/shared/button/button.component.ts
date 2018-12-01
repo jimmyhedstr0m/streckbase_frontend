46377465732214704f56edf1bff936a4a69ff3d1
Jimmy Hedström <jimmy.hedstrom93@gmail.com>
2018-12-01 23:39:58 +0100

    Started with sass variables

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-button",
@@ -6,6 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./button.component.scss"]
 })
 export class ButtonComponent {
+  @Input() modifiers: string[] = [];
 
   constructor() { }
 
