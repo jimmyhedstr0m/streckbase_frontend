85b3ea2c9113c59012fe789ebcef12e0cf2d3d3d
Jimmy Hedström <jimmy.hedstrom93@gmail.com>
2018-12-02 21:55:30 +0100

    Clock styling

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -8,15 +8,33 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 export class ClockComponent implements OnInit, OnDestroy {
   private interval: number = 1000;
   private timer: any;
+  private minutesPerRevolution: number = 12 * 60;
+  private minuteCircumference: number = 100;
+  private hourCircumference: number;
   public currentTime: Date;
+  public hourStrokeArray: string;
+  public minutesStrokeArray: string;
+  public strokeWidth: number = 3;
+  public hourOffset: number;
+  public minuteOffset: number;
+  public minuteRadius: number;
+  public hourRadius: number;
 
-  constructor() { }
+  constructor() {
+    this.minuteRadius = this.minuteCircumference / (2 * Math.PI);
+    this.minuteOffset = this.minuteCircumference * .25;
+    this.hourRadius = (this.minuteCircumference / Math.PI - 2 * this.strokeWidth) / 2;
+    this.hourCircumference = 2 * this.hourRadius * Math.PI;
+    this.hourOffset = this.hourCircumference * .25;
+  }
 
   ngOnInit() {
     this.currentTime = new Date();
+    this.calculateTimePercentage();
 
     this.timer = setInterval(() => {
       this.currentTime = new Date();
+      this.calculateTimePercentage();
     }, this.interval);
   }
 
@@ -24,4 +42,13 @@ export class ClockComponent implements OnInit, OnDestroy {
     clearInterval(this.timer);
   }
 
+  private calculateTimePercentage() {
+    const currentMinutes: number = this.currentTime.getMinutes();
+    const currentHour: number = (this.currentTime.getHours() % 12) * 60;
+    const hourPercentage: number = currentHour / this.minutesPerRevolution;
+    const minutesPercentage: number = currentMinutes / 60;
+
+    this.minutesStrokeArray = `${minutesPercentage * this.minuteCircumference} ${(1 - minutesPercentage) * this.minuteCircumference}`;
+    this.hourStrokeArray = `${hourPercentage * this.hourCircumference} ${(1 - hourPercentage) * this.hourCircumference}`;
+  }
 }
