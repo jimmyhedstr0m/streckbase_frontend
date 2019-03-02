6fdbc2eb9c7a6ff76af4a46682adac1bb1d3d4de
Jimmy Hedström <jimmy.hedstrom93@gmail.com>
2019-03-03 00:03:01 +0100

    Resolved party page bug

--- src/app/pages/front/front.component.ts
+++ src/app/pages/front/front.component.ts
@@ -34,7 +34,6 @@ export class FrontComponent implements OnInit, OnDestroy {
       )
       .subscribe((highscoreUsers: User[]) => {
         this.highscoreUsers = highscoreUsers;
-        console.log(this.highscoreUsers);
       });
   }
 
--- src/app/pages/party/party.component.ts
+++ src/app/pages/party/party.component.ts
@@ -1,4 +1,8 @@
 import { Component } from "@angular/core";
+import { Subscription } from "rxjs/internal/Subscription";
+
+import { FrontService } from "./../front/front.service";
+import { User } from "./../../types/user";
 
 @Component({
   selector: "app-party",
@@ -6,7 +10,17 @@ import { Component } from "@angular/core";
   styleUrls: ["./party.component.scss"]
 })
 export class PartyComponent {
+  private feedSubscription: Subscription;
+  public feed: User[];
 
-  constructor() { }
+  constructor(private frontService: FrontService) { }
 
+  ngOnInit() {
+    this.feedSubscription = this.frontService.getFeed()
+      .subscribe((feed: User[]) => this.feed = feed);
+  }
+
+  ngOnDestroy() {
+    this.feedSubscription.unsubscribe();
+  }
 }
