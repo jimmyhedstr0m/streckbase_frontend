086f21aa3a0e68f1b1616d488c7be628e859f7af
Jimmy Hedström <jimmy.hedstrom93@gmail.com>
2019-03-06 15:42:45 +0100

    Added small cache to users to speed up menu

--- src/app/pages/users/users.service.ts
+++ src/app/pages/users/users.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs/internal/Observable";
+import { ReplaySubject } from "rxjs/internal/ReplaySubject";
 
 import { environment } from "./../../../environments/environment";
 import { User } from "./../../types/user";
 
 @Injectable()
 export class UsersService {
+  private usersSubject = new ReplaySubject<User[]>(1);
 
   constructor(private http: HttpClient) { }
 
+  private fetchUsers() {
+    this.http.get<User[]>(`${environment.apiUrl}/users?limit=1000`)
+      .subscribe((users: User[]) => this.usersSubject.next(users));
+  }
+
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`${environment.apiUrl}/users?limit=1000`);
+    this.fetchUsers();
+    return this.usersSubject.asObservable();
   }
 }
