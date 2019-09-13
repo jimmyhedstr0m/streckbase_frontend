74615ddcdf9b0f5bb18c50bf6550661a4e2985e6
Ljud och Bildgruppen <LoB@LOBmini1.local>
2019-09-13 20:59:42 +0200

    Added debt heat map

--- src/app/shared/user-card/user-card.component.ts
+++ src/app/shared/user-card/user-card.component.ts
@@ -10,6 +10,7 @@ import { User } from "./../../types/user";
 export class UserCardComponent {
   @Input() includeName = true;
   @Input() user: User;
+  @Input() showHeat: boolean = false;
   private colorClasses = ["purple", "orange", "blue", "mint", "whine", "police", "blod"];
 
   constructor() { }
@@ -17,6 +18,18 @@ export class UserCardComponent {
   getColorClass(): string {
     const index = (parseInt(this.user.id, 10) + this.user.firstname.length) % this.colorClasses.length;
     return this.colorClasses[index];
+  }
+
+  getHeatClass(): string {
+    if (this.user.debt < 499) {
+      return "neutral";
+    } else if (this.user.debt < 999) {
+      return "warning";
+    } else if (this.user.debt >= 1000) {
+      return "danger";
+    } else {
+      return "";
+    }
   }
 
 }
